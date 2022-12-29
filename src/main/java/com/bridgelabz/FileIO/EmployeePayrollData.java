a2e50fcf4139688ad4662d5da0face590e0535f5
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-29 22:19:52 +0530

    Added: Create an EmployeePayroll Service to Read

---
+++ src/main/java/com/bridgelabz/FileIO/EmployeePayrollData.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.FileIO;
+
+public class EmployeePayrollData {
+    int id;
+    String name;
+    double salary;
+
+    EmployeePayrollData() {
+    }
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/main/java/com/bridgelabz/FileIO/EmployeePayrollService.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.FileIO;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService<T> {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public enum IOService {
+        File_IO, Console_IO;
+    }
+
+    List<EmployeePayrollData> payrollList;
+
+    EmployeePayrollService(List<EmployeePayrollData> payrollList) {
+        this.payrollList = payrollList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        employeePayrollService.writeEmployeePayrollData();
+        employeePayrollService.readEmployeePayrollData();
+    }
+
+    private void writeEmployeePayrollData() {
+        System.out.println("Enter Employee id");
+        int id = sc.nextInt();
+        System.out.println("Enter Employee name");
+        String name = sc.next();
+        System.out.println("Enter Employee Salary");
+        double salary = sc.nextDouble();
+
+        payrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    private void readEmployeePayrollData() {
+        Iterator iterator = payrollList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next() + " ");
+        }
+    }
+}
